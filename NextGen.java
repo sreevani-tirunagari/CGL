ff5a5c23a2ca735a7deb2c66a5afbafa590a2083
sreevani-tirunagari <72122307+sreevani-tirunagari@users.noreply.github.com>
2020-12-25 18:48:35 +0530

    Third Commit
    Uploaded all files

--- NextGen.java
+++ NextGen.java
@@ -1,9 +1,11 @@
 package cgl;
 
 public class NextGen {
-	public static void main(String Args[]) {
-		System.out.println("Hello I'm Next GEN!");
-		System.out.println("Hello I'm Next GEN!");
+
+	public static void main(String[] args) {
+		System.out.println("Hello I'm Next Gen!");
+		System.out.println("Hello I'm Next Gen!");
+
 	}
 
 }
